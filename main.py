59ddc706d72ea77a62e4f1259aee2c1e3bacc1f8
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-02-18 23:24:23 -0300

    Now getting the bulletin content

--- main.py
+++ main.py
@@ -15,12 +15,13 @@ load_dotenv()
 EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
 USER_ID = os.getenv("USER_ID")
-WEBSITE = "https://spacehey.com/userbulletins?id="+USER_ID
+SPACEHEY_URL = "https://spacehey.com/"
+BULLETINS_SITE = SPACEHEY_URL + "userbulletins?id=" + USER_ID
 
 options = Options()
 
 # don't open the broser
-options.add_argument("--headless")
+#options.add_argument("--headless")
 
 # don't kills chrome when code ends
 #options.add_experimental_option("detach", True) 
@@ -29,8 +30,8 @@ options.add_argument("--headless")
 # Donwload driver (If needed) and starts it
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get(WEBSITE)
-driver.implicitly_wait(2)
+driver.get(BULLETINS_SITE)
+driver.implicitly_wait(3)
 
 email_field = driver.find_element(By.ID, "email")
 email_field.send_keys(EMAIL)
@@ -42,11 +43,11 @@ pswrd_field.send_keys(PASSWORD)
 pswrd_field.submit()
 pswrd_field.submit()
 
-driver.get(WEBSITE)
+driver.get(BULLETINS_SITE)
 
 ### now its in the logged in and on the right website ###
 
-bulletin_info = {}
+bulletins_info = {}
 ''' How it'll look like
 "Title": {
     "bulletin-text":"text",
@@ -59,8 +60,8 @@ bulletin_info = {}
 }
 '''
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-bulletin_table = soup.find("table", {"class":"bulletin-table"})
+bulletins_soup = BeautifulSoup(driver.page_source, 'html.parser')
+bulletin_table = bulletins_soup.find("table", {"class":"bulletin-table"})
 bulletins = bulletin_table.findAll("tr")
 for bulletin in bulletins[1:]:
     title = bulletin.find("td", {"class":"subject"})
@@ -72,10 +73,21 @@ for bulletin in bulletins[1:]:
 
     comments_count = bulletin.findAll("td")[2].get_text().replace("\n", "")[0]
 
-    bulletin_info[title] = {
-        "comments-coun":int(comments_count),
-        "bulletin-date":time
-        }
-    bulletin_link = bulletin.find("a", href=True)["href"]
+    bulletin_link = SPACEHEY_URL + bulletin.find("a", href=True)["href"]
 
-print(bulletin_info)
+    driver.get(bulletin_link)
+
+    bulletin_soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    bulletin_content = bulletin_soup.find("div", {"class":"content"})
+    bulletin_content = bulletin_content.get_text().replace("\n", "")
+
+    bulletins_info[title] = {
+        "comments-coun":int(comments_count),
+        "bulletin-date":time,
+        "content":bulletin_content
+        }
+    driver.get(BULLETINS_SITE)
+    
+
+print(bulletins_info)
